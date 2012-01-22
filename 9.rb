22c86bdb316b5110c8989143ad23a62f783d3466
Alex Egg <eggie5@gmail.com>
2012-01-22 14:37:35 -0800

    Update 9.rb

--- 9.rb
+++ 9.rb
@@ -1,13 +1,13 @@
-A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+#A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
 
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+#a2 + b2 = c2
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
 
-There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
 
 
-I just did the math on paper w/ this one then had wolfram alpha spit out the possible answers of the system.
+#I just did the math on paper w/ this one then had wolfram alpha spit out the possible answers of the system.
 
-http://www.wolframalpha.com/input/?i=a%5E2%2Bb%5E2-c%5E2%3D0%2C+a%2Bb%2Bc%3D1000%2C+a%3Cb%3Cc
+#http://www.wolframalpha.com/input/?i=a%5E2%2Bb%5E2-c%5E2%3D0%2C+a%2Bb%2Bc%3D1000%2C+a%3Cb%3Cc
 
